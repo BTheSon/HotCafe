bdf13167eadff9486cd8913da3a6f29423d7a932
Bui_The_Son <buitheson201@gmail.com>
2025-12-09 14:17:15 +0700

    update: hiển thị danh sách tin nhắn trong room

--- wwwroot/Chat.js
+++ wwwroot/Chat.js
@@ -1,15 +1,61 @@
-function Chat({ match }) {
-    const roomId = match.params.id;
-    /// fetch("/api/rooms/members/get-list", {
-    ///		method: "POST",
-    ///		headers: {
-    ///			"Content-Type": "application/json",
-    ///			"room_id": "roomid"
-    ///		}
-    ///	});
+const { useEffect, useState } = React;
+
+function MessageComponent({ username, mess, date_send }) {
     return (
         <div>
-
+            <p>{username}</p>
+            <p>{mess}</p>
+            <p>{date_send}</p>
         </div>
-    )
+    );
+}
+
+function Chat({ match }) {
+    const roomId = match.params.id;
+    const [listMessager, setListMessager] = useState([]);
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            try {
+                const response = await fetch("/api/rooms/messages/get-list", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "room_id": roomId
+                    }
+                });
+
+                // 1. Dùng .text() để lấy nội dung
+                const rawDataString = await response.text();
+
+                // 2. Tự parse chuỗi đó thành JSON object
+                // **Đây là bước bắt buộc vì Server trả về text/plain**
+                const data = JSON.parse(rawDataString);
+
+                setListMessager(data);
+            } catch (err) {
+                console.error("some error idk", err);
+            }
+        };
+
+        fetchData();
+    }, [roomId]);
+
+    return (
+        <div>
+            <p>chatpae</p>
+            <div>
+                {listMessager?.map(item => (
+                    <MessageComponent
+                        key={item.id}
+                        username={item.username}
+                        mess={item.mess}
+                        date_send={item.date_send}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
